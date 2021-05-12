c4511a800a4c4c59a7dfa626b762475ac3619d4f
Britta Ennus <britta@ennus.ee>
2021-05-12 09:24:25 +0300

    commit

--- src/test/java/selenium/SeleniumTest1.java
+++ src/test/java/selenium/SeleniumTest1.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class SeleniumTest1 {
 
     public static void main(String[] args) {
@@ -12,6 +14,7 @@ public class SeleniumTest1 {
         System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("https://www.rik.ee/et");
         WebElement element = driver.findElement(By.name("search_block_form"));
         element.sendKeys("tööpakkumised\n");
